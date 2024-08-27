551255ec420e9eb0dc6cf059e48a65f6a2a7e87b
OJIMA Ryoji <rojima1@gmail.com>
2024-08-27 20:41:45 +0900

    017

--- src/model.py
+++ src/model.py
@@ -11,7 +11,7 @@ def create_model():
         return keras.layers.Add()
 
     def Conv(filters):
-        return keras.layers.Conv1D(filters, 3, padding='same', use_bias=False, kernel_initializer=keras.initializers.HeNormal())
+        return keras.layers.Conv1D(filters, 5, padding='same', use_bias=False, kernel_initializer=keras.initializers.HeNormal())
 
     def DepthwiseConv(depth_multiplier=1):
         return keras.layers.DepthwiseConv1D(3, padding='same', depth_multiplier=depth_multiplier, use_bias=False, depthwise_initializer=keras.initializers.HeNormal())
