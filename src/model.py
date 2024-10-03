1cd4abc0272481c61c66afeb4ca6f840bc73d26f
OJIMA Ryoji <rojima1@gmail.com>
2024-10-04 05:50:40 +0900

    069

--- src/model.py
+++ src/model.py
@@ -111,7 +111,7 @@ def create_model():
         for _ in range(5):
             x = DepthwiseConvUnit0()(x)
 
-            for _ in range(5 - 1):
+            for _ in range(4 - 1):
                 x = DepthwiseConvUnit()(x)
 
             x = Pooling()(x)
@@ -121,7 +121,7 @@ def create_model():
         for filters in (256, 128, 64, 32, 3):
             x = ConvUnit0(filters)(x)
 
-            for _ in range(5 - 1):
+            for _ in range(4 - 1):
                 x = ConvUnit(filters)(x)
 
         return x
